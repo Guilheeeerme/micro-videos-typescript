d00de4aec92bb07ffe55e25fc2d0e3d806eebef8
Guilheeeerme <guilherme_moura.10222@outlook.com>
2023-05-05 16:26:49 -0300

    feat: adicionando as regras de validação na entidade Categoria

--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -1,3 +1,4 @@
+import ValidatorRules from "../../../@seedwork/domain/validators/validator-rules";
 import Entity from "../../../@seedwork/domain/entity/entity";
 import UniqueEntityId from "../../../@seedwork/domain/value-objects/unique-entity-id.vo";
 
@@ -10,6 +11,7 @@ export type CategoryProperties = {
 
 export class Category extends Entity<CategoryProperties> {
   constructor(public readonly props: CategoryProperties, id?: UniqueEntityId) {
+    Category.validate(props);
     super(props, id);
     this.description = this.props.description;
     this.props.is_active = this.props.is_active ?? true;
@@ -17,8 +19,18 @@ export class Category extends Entity<CategoryProperties> {
   }
 
   update(name: string, description: string): void {
+    Category.validate({
+      name,
+      description,
+    });
     this.name = name;
     this.description = description;
+  }
+
+  static validate(props: Omit<CategoryProperties, "created_at">) {
+    ValidatorRules.values(props.name, "name").required().string();
+    ValidatorRules.values(props.description, "description").string();
+    ValidatorRules.values(props.is_active, "is_active").boolean();
   }
 
   activate() {
