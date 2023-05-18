a854ff697615f40c5b86e497d01122f0bfbad476
Guilheeeerme <guilherme_moura.10222@outlook.com>
2023-05-18 17:47:39 -0300

    feat: Resolução "Desafio -Repositório e validação da entidade Categoria"

--- src/category/infra/repository/category-in-memory.repository.ts
+++ src/category/infra/repository/category-in-memory.repository.ts
@@ -31,6 +31,8 @@ export default class CategoryInMemoryRepository
     throw new Error("Method not implemented.");
   }
 
+  sortableFields: string[] = ["name", "created_at"];
+
   protected async applyFilter(
     items: Category[],
     filter: CategoryRepository.Filter
@@ -42,5 +44,15 @@ export default class CategoryInMemoryRepository
     return items.filter((i) => {
       return i.props.name.toLowerCase().includes(filter.toLowerCase());
     });
+  }
+
+  protected async applySort(
+    items: Category[],
+    sort: string | null,
+    sort_dir: SortDirection | null
+  ): Promise<Category[]> {
+    return !sort
+      ? super.applySort(items, "created_at", "desc")
+      : super.applySort(items, sort, sort_dir);
   }
 }
